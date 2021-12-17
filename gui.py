9a45df820a05125d5136d85c885f34158bb86c36
Razyar Saeedian <razyarsaeedian@gmail.com>
2021-12-17 20:59:38 +0330

    New ui (Not Connected to main code)

--- gui.py
+++ gui.py
@@ -20,8 +20,8 @@ class Ui_MainWindow(object):
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("ico.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
-        MainWindow.setStyleSheet("background-color: rgb(160, 169, 179);\n"
-"\n"
+        MainWindow.setStyleSheet("background-color: rgb(1, 1, 1);\n"
+"color: rgb(255, 255, 255);\n"
 "border: 0px solid;\n"
 "border-radius: 10px;")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -29,26 +29,27 @@ class Ui_MainWindow(object):
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
         self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setEnabled(True)
         font = QtGui.QFont()
         font.setFamily("Big John PRO Light")
         font.setBold(True)
         font.setWeight(75)
         self.frame.setFont(font)
         self.frame.setStyleSheet("color: rgb(0, 0, 0);\n"
-"\n"
-"background-image: url(:/1/bg.jpg);\n"
-"")
+"background-image: url(:/1/bg.jpg);")
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
         self.lineEdit_2 = QtWidgets.QLineEdit(self.frame)
-        self.lineEdit_2.setGeometry(QtCore.QRect(230, 250, 201, 21))
+        self.lineEdit_2.setGeometry(QtCore.QRect(190, 360, 311, 21))
         self.lineEdit_2.setStyleSheet("QLineEdit {\n"
 "    border: 0px solid;\n"
 "        border-radius: 10px;\n"
 "    \n"
-"    color: rgb(170, 0, 0);\n"
+"    \n"
+"    color: rgb(1, 1, 1);\n"
 "    font: 8pt \"Big John PRO Light\";\n"
+"    \n"
 "    \n"
 "    background-image: url(:/1/icons/btn.png);\n"
 "\n"
@@ -62,7 +63,7 @@ class Ui_MainWindow(object):
 "}")
         self.lineEdit_2.setObjectName("lineEdit_2")
         self.pushButton_2 = QtWidgets.QPushButton(self.frame)
-        self.pushButton_2.setGeometry(QtCore.QRect(10, 430, 51, 43))
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 420, 51, 43))
         font = QtGui.QFont()
         font.setFamily("Trebuchet MS")
         font.setPointSize(11)
@@ -84,25 +85,18 @@ class Ui_MainWindow(object):
 "}")
         self.pushButton_2.setText("")
         self.pushButton_2.setObjectName("pushButton_2")
-        self.label = QtWidgets.QLabel(self.frame)
-        self.label.setGeometry(QtCore.QRect(190, 250, 31, 21))
-        font = QtGui.QFont()
-        font.setFamily("Big John PRO Light")
-        font.setBold(True)
-        font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setStyleSheet("color: rgb(0, 0, 0);")
-        self.label.setObjectName("label")
         self.label_3 = QtWidgets.QLabel(self.frame)
-        self.label_3.setGeometry(QtCore.QRect(280, 460, 181, 21))
+        self.label_3.setGeometry(QtCore.QRect(10, 460, 161, 21))
         font = QtGui.QFont()
         font.setFamily("Big John PRO")
         font.setBold(True)
         font.setWeight(75)
         self.label_3.setFont(font)
+        self.label_3.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgb(252, 68, 68);")
         self.label_3.setObjectName("label_3")
         self.pushButton_6 = QtWidgets.QPushButton(self.frame)
-        self.pushButton_6.setGeometry(QtCore.QRect(440, 250, 24, 21))
+        self.pushButton_6.setGeometry(QtCore.QRect(480, 360, 21, 21))
         self.pushButton_6.setStyleSheet("QPushButton{\n"
 "    border: 0px solid;\n"
 "        border-radius: 10px;\n"
@@ -118,113 +112,15 @@ class Ui_MainWindow(object):
 "}")
         self.pushButton_6.setText("")
         self.pushButton_6.setObjectName("pushButton_6")
-        self.label_2 = QtWidgets.QLabel(self.frame)
-        self.label_2.setGeometry(QtCore.QRect(190, 280, 31, 21))
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(190, 390, 131, 20))
         font = QtGui.QFont()
         font.setFamily("Big John PRO Light")
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_2.setFont(font)
-        self.label_2.setStyleSheet("color: rgb(0, 0, 0);")
-        self.label_2.setObjectName("label_2")
-        self.lineEdit_3 = QtWidgets.QLineEdit(self.frame)
-        self.lineEdit_3.setGeometry(QtCore.QRect(230, 280, 201, 21))
-        self.lineEdit_3.setStyleSheet("QLineEdit {\n"
-"    border: 0px solid;\n"
-"        border-radius: 10px;\n"
-"    \n"
-"    color: rgb(170, 0, 0);\n"
-"    font: 8pt \"Big John PRO Light\";\n"
-"    \n"
-"    background-image: url(:/1/icons/btn.png);\n"
-"\n"
-"}\n"
-"\n"
-"QLineEdit:hover {\n"
-"    \n"
-"\n"
-"\n"
-"\n"
-"}")
-        self.lineEdit_3.setObjectName("lineEdit_3")
-        self.pushButton_7 = QtWidgets.QPushButton(self.frame)
-        self.pushButton_7.setGeometry(QtCore.QRect(440, 280, 24, 21))
-        self.pushButton_7.setStyleSheet("QPushButton{\n"
-"    border: 0px solid;\n"
-"        border-radius: 10px;\n"
-"    color: rgb(0, 0, 0);\n"
-"    background-image: url(:/1/icons/cil-check-alt.png);\n"
-"\n"
-"}\n"
-"\n"
-"QPushButton:hover {\n"
-"\n"
-"\n"
-"\n"
-"}")
-        self.pushButton_7.setText("")
-        self.pushButton_7.setObjectName("pushButton_7")
-        self.label_4 = QtWidgets.QLabel(self.frame)
-        self.label_4.setGeometry(QtCore.QRect(190, 310, 31, 21))
-        font = QtGui.QFont()
-        font.setFamily("Big John PRO Light")
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_4.setFont(font)
-        self.label_4.setStyleSheet("color: rgb(0, 0, 0);")
-        self.label_4.setObjectName("label_4")
-        self.lineEdit_4 = QtWidgets.QLineEdit(self.frame)
-        self.lineEdit_4.setGeometry(QtCore.QRect(230, 310, 201, 21))
-        self.lineEdit_4.setStyleSheet("QLineEdit {\n"
-"    border: 0px solid;\n"
-"        border-radius: 10px;\n"
-"    \n"
-"    color: rgb(170, 0, 0);\n"
-"    font: 8pt \"Big John PRO Light\";\n"
-"    \n"
-"    background-image: url(:/1/icons/btn.png);\n"
-"\n"
-"}\n"
-"\n"
-"QLineEdit:hover {\n"
-"    \n"
-"\n"
-"\n"
-"\n"
-"}")
-        self.lineEdit_4.setObjectName("lineEdit_4")
-        self.pushButton_8 = QtWidgets.QPushButton(self.frame)
-        self.pushButton_8.setGeometry(QtCore.QRect(440, 310, 24, 21))
-        self.pushButton_8.setStyleSheet("QPushButton{\n"
-"    border: 0px solid;\n"
-"        border-radius: 10px;\n"
-"    color: rgb(0, 0, 0);\n"
-"    background-image: url(:/1/icons/cil-check-alt.png);\n"
-"\n"
-"}\n"
-"\n"
-"QPushButton:hover {\n"
-"\n"
-"\n"
-"\n"
-"}")
-        self.pushButton_8.setText("")
-        self.pushButton_8.setObjectName("pushButton_8")
-        self.label_5 = QtWidgets.QLabel(self.frame)
-        self.label_5.setGeometry(QtCore.QRect(200, 170, 201, 61))
-        font = QtGui.QFont()
-        font.setFamily("Big John PRO Bold")
-        font.setPointSize(32)
-        self.label_5.setFont(font)
-        self.label_5.setObjectName("label_5")
-        self.label_6 = QtWidgets.QLabel(self.frame)
-        self.label_6.setGeometry(QtCore.QRect(400, 170, 111, 61))
-        font = QtGui.QFont()
-        font.setFamily("Big John PRO Bold")
-        font.setPointSize(32)
-        self.label_6.setFont(font)
-        self.label_6.setStyleSheet("color: rgb(170, 0, 0);")
-        self.label_6.setObjectName("label_6")
+        font.setBold(False)
+        font.setWeight(50)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.frame)
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -234,12 +130,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Pointer"))
-        self.label.setText(_translate("MainWindow", "Point"))
         self.label_3.setText(_translate("MainWindow", "Say everything, Fuck everyone."))
-        self.label_2.setText(_translate("MainWindow", "Time"))
-        self.label_4.setText(_translate("MainWindow", "Gift"))
-        self.label_5.setText(_translate("MainWindow", "P O I N T"))
-        self.label_6.setText(_translate("MainWindow", "E R"))
+        self.label.setText(_translate("MainWindow", "ex: Gym, 365 days, 200$"))
 import qrc_rc
 
 
